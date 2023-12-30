3a8b22771af7d844b9c21cf5edf63c0c12d6a1a1
Mateus Adamo <adamomateus478@gmail.com>
2023-12-30 01:12:22 -0300

    feat: Creation of Forms.

--- src/Screens/Form.tsx
+++ src/Screens/Form.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image, TouchableOpacity, Text, TextInput } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text, TextInput } from 'react-native';
 
 
 
@@ -16,11 +15,20 @@ export default function Form() {
                 />
             </View>
             <View style={styles.boxContact}>
-                <Text style={styles.labelContact}>Nome</Text>
+                <Text style={styles.labelContact}>Contact</Text>
                 <TextInput style={styles.boxTextInputContact}
                     placeholder=' Insira seu nome: '
                 />
             </View>
+            <View style={styles.boxDescription}>
+                <Text style={styles.labelDescription}>Descrição</Text>
+                <TextInput style={styles.boxTextInputDescription}
+                    placeholder='Descreva o caso: '
+                />
+            </View>
+            <TouchableOpacity style={styles.btnSend}>
+                <Text style={styles.BtnText}>Enviar</Text>
+            </TouchableOpacity>
 
         </View>
     );
@@ -37,7 +45,7 @@ const styles = StyleSheet.create({
         width: '80%',
         height: '14%',
         paddingLeft: 15,
-        bottom: '26%',
+        bottom: '14%',
     },
 
     labelName: {
@@ -57,7 +65,7 @@ const styles = StyleSheet.create({
         width: '80%',
         height: '14%',
         paddingLeft: 15,
-        bottom: '24%',
+        bottom: '14%',
 
     },
 
@@ -73,6 +81,45 @@ const styles = StyleSheet.create({
         borderRadius: 14,
         width: '96%',
     },
+    /////////////////////////////
+    boxDescription: {
+        width: '80%',
+        height: '20%',
+        paddingLeft: 15,
+        bottom: '14%',
+
+    },
+
+    labelDescription: {
+        fontWeight: 'bold',
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+
+    boxTextInputDescription: {
+        borderWidth: 1.5,
+        padding: 5,
+        borderRadius: 14,
+        paddingTop: 12,
+        width: '96%',
+        height: 100,
+        textAlignVertical: 'top',
+
+    },
+    //////////////////////////////
+    btnSend: {
+        backgroundColor: 'black',
+        padding: 12,
+        borderRadius: 20,
+        bottom: '5%',
+        width: '72%'
+    },
+    BtnText: {
+        color: 'white',
+        fontWeight: '900',
+        fontSize: 20,
+        textAlign: 'center'
+    }
 
 
 });
